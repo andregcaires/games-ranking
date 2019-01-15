5e0d7c2e71bb633e3f7bfc4929f2b74375d3c60e
andre.caires <andre.caires@trust.com.br>
2019-01-15 16:49:58 -0200

    Teste

--- Back/src/test/java/com/andregcaires/gamesranking/services/JogadorServiceTest.java
+++ Back/src/test/java/com/andregcaires/gamesranking/services/JogadorServiceTest.java
@@ -1,5 +1,7 @@
 package com.andregcaires.gamesranking.services;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Assert;
@@ -19,11 +21,15 @@ public class JogadorServiceTest {
 	@Mock
 	private JogadorRepository _repo;
 	
-	private Jogador j1;
+	private Jogador j1, j2, j3;
+	private List<Jogador> listJogadores;
 	
 	@Before
 	public void init() {
 		j1 = new Jogador(1, "Carlos", 0, 0);
+		j2 = new Jogador(null, "Maria", 0, 0);
+		j3 = new Jogador(null, "José", 0, 0);
+		listJogadores.addAll(Arrays.asList(j1, j2, j3));
 	}
 	
 	@Test
@@ -33,5 +39,14 @@ public class JogadorServiceTest {
 		Jogador jogadorTest = opt.orElse(null);
 		
 		Assert.assertEquals(j1, jogadorTest);
+	}
+	
+	// realizar no perfil dev - banco de dados MySql
+	@Test
+	public void verificaPesquisaTodosJogadores() {
+		
+		List<Jogador> listaBanco = _repo.findAll();
+		
+		Assert.assertEquals(listJogadores, listaBanco);
 	}
 }
